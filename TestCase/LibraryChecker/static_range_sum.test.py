f4db7cdf2322d9bc566559ffba7526c51f84a30f
Neterukun1993 <v5del11138@gmail.com>
2021-01-01 20:17:04 +0900

    add static_range_sum.test.py

---
+++ TestCase/LibraryChecker/static_range_sum.test.py
@@ -0,0 +1,18 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/static_range_sum
+import sys
+input = sys.stdin.buffer.readline
+
+from Library.DataStructure.AccumulateSum.AccumulateSum import AccumulateSum
+
+
+n, q = map(int, input().split())
+a = list(map(int, input().split()))
+queries = [list(map(int, input().split())) for _ in range(q)]
+
+
+acc = AccumulateSum(a)
+ans = []
+for l, r in queries:
+    ans.append(acc.sum(l, r))
+
+print('\n'.join(map(str, ans)))

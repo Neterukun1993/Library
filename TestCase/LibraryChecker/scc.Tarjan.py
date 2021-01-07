a0997910d115b324be3a768d929cef8d5201e12b
Neterukun1993 <v5del11138@gmail.com>
2021-01-08 06:36:19 +0900

    create SCC_Tarjan.py

---
+++ TestCase/LibraryChecker/scc.Tarjan.py
@@ -0,0 +1,24 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/scc
+import sys
+input = sys.stdin.buffer.readline
+
+from Graph.Decomposition.SCC_Tarjan import StronglyConnectedComponents
+
+
+def main():
+    n, m = map(int, input().split())
+    edges = [list(map(int, input().split())) for i in range(m)]
+
+    scc = StronglyConnectedComponents(n)
+    for v, nxt_v in edges:
+        scc.add_edge(v, nxt_v)
+    scc.build()
+    _, elems = scc.construct_dag()
+
+    print(len(elems))
+    for res in elems:
+        print(len(res), *res)
+
+
+if __name__ == '__main__':
+    main()
---
+++ docs/Graph/Decomposition/SCC_Tarjan.md
@@ -0,0 +1,4 @@
+---
+title: 強連結成分分解 (Tarjanのアルゴリズム)
+documentation_of: //Graph/Decomposition/SCC_Tarjan.py
+---

29b885139ef937f6e83786d0c3885d6070696579
Neterukun1993 <v5del11138@gmail.com>
2021-01-22 20:39:37 +0900

    add runlength_compress

---
+++ String/runlength_compress.py
@@ -0,0 +1,13 @@
+def compress(string):
+    n = len(string)
+    begin, cnt = 0, 0
+    ans = []
+    if n == 0:
+        return ans
+    for end in range(n + 1):
+        if end == n or string[begin] != string[end]:
+            ans.append((string[begin], cnt))
+            begin, cnt = end, 1
+        else:
+            cnt += 1
+    return ans

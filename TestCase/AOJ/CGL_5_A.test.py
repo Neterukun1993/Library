39515a8fce68c5c9b1b5eaa0cd6469e0fb87768d
Neterukun1993 <v5del11138@gmail.com>
2021-02-07 11:37:03 +0900

    add closest_pair

---
+++ TestCase/AOJ/CGL_5_A.test.py
@@ -0,0 +1,17 @@
+# verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_5_A
+import sys
+input = sys.stdin.buffer.readline
+
+from Geometry.closest_pair import closest_pair
+
+
+def main():
+    n = int(input())
+    points = [list(map(float, input().split())) for i in range(n)]
+
+    dist, *other = closest_pair(points)
+    print(dist)
+
+
+if __name__ == '__main__':
+    main()

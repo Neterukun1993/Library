93fecbdc57f639965beed75d38aeb9f7dc08a29e
Neterukun1993 <v5del11138@gmail.com>
2021-09-05 23:28:54 +0900

    fix lis

--- DP/lis.py
+++ DP/lis.py
@@ -3,10 +3,13 @@ from bisect import bisect_left, bisect_right
 
 def lis(array, strict=True):
     lis_array = [array[0]]
+    bisect_ = bisect_left if strict else bisect_right
+
     for val in array[1:]:
-        if val > lis_array[-1] or (not strict and val == lis_array):
+        idx = bisect_(lis_array, val)
+        if idx == len(lis_array):
             lis_array.append(val)
         else:
-            lis_array[bisect_left(lis_array, val)] = val
+            lis_array[idx] = val
 
     return lis_array

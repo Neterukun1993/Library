d0e58874ff051ff5be3440dbe30e4e22648b6528
Neterukun1993 <v5del11138@gmail.com>
2022-08-07 15:04:38 +0900

    fix bitvector

--- DataStructure/Wavelet/BitVector.py
+++ DataStructure/Wavelet/BitVector.py
@@ -1,18 +1,11 @@
 class BitVector:
-    def __init__(self, size):
+    def __init__(self, n):
         # self.BLOCK_WIDTH = 32
-        self.BLOCK_NUM = (size + 31) >> 5
+        self.BLOCK_NUM = (n + 31) >> 5
         self.bit = [0] * self.BLOCK_NUM
         self.count = [0] * self.BLOCK_NUM
 
-    def set(self, i):
-        self.bit[i >> 5] |= 1 << (i & 31)
-
-    def build(self):
-        for i in range(self.BLOCK_NUM - 1):
-            self.count[i + 1] = self.count[i] + self.popcount(self.bit[i])
-
-    def popcount(self, x):
+    def _popcount(self, x):
         x = x - ((x >> 1) & 0x55555555)
         x = (x & 0x33333333) + ((x >> 2) & 0x33333333)
         x = (x + (x >> 4)) & 0x0F0F0F0F
@@ -20,9 +13,16 @@ class BitVector:
         x = x + (x >> 16)
         return x & 0x0000007F
 
+    def set(self, i):
+        self.bit[i >> 5] |= 1 << (i & 31)
+
     def access(self, i):
         return (self.bit[i >> 5] >> (i & 31)) & 1
 
+    def build(self):
+        for i in range(self.BLOCK_NUM - 1):
+            self.count[i + 1] = self.count[i] + self._popcount(self.bit[i])
+
     def rank(self, r, f):
-        res = self.count[r >> 5] + self.popcount(self.bit[r >> 5] & ((1 << (r & 31)) - 1))
+        res = self.count[r >> 5] + self._popcount(self.bit[r >> 5] & ((1 << (r & 31)) - 1))
         return res if f else r - res
